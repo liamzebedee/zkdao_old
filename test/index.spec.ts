13f9c1ffccea190565850a7c5d25fe77c5ac0436
liamzebedee <liamzebedee@yahoo.com.au>
2019-03-08 22:47:55 +0100

    structure

---
+++ test/index.spec.ts
@@ -0,0 +1,12 @@
+import chai, { expect, should, assert } from 'chai';
+import { describe, it, setup, teardown } from 'mocha';
+// import { bufferToHex, toBuffer  } from 'ethereumjs-util';
+import Web3 from 'web3';
+
+import chaiAsPromised from 'chai-as-promised';
+chai.use(chaiAsPromised);
+import 'mocha';
+
+describe('ZKDAO', () => {
+    
+})
